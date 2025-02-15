6f54c30cb39861469ddf480f513b4851c6dd68ca
LEEBAN SESARIO S <143171342+Leebansesario@users.noreply.github.com>
2025-02-15 11:36:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Don't Remove this code until template is crash
+  document.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+  });
+  
+  // Don't Remove this code until template is crash
